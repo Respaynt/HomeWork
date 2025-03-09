3ca544484fedbcb01f3f48861b336e31e15a37f3
Первый комит <suzdalcev97t@gmail.com>
2025-03-09 23:22:51 +0400

    НОВЫЕ ИЗМЕНЕНИЯ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//ЧТО ЕЩЕ МНЕ НУЖНО СДЕЛАТЬ!!!!???
+
 public class Main {
     public static void main(String[] args){
         var dog = 8.0;
